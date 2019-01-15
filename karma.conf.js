553e9b080d1e25bddd692617627cb241d40f0db1
alanclarke <alan@qubit.com>
2019-01-15 11:56:35 +0000

    Add a minimum code coverage requirement

--- karma.conf.js
+++ karma.conf.js
@@ -3,13 +3,9 @@ var path = require('path')
 
 module.exports = function (config) {
   config.set({
-    frameworks: ['mocha'],
-    files: [
-      'test/*test.js'
-    ],
-    preprocessors: {
-      '/**/*.js': ['webpack', 'sourcemap']
-    },
+    frameworks: [ 'mocha' ],
+    files: [ 'test/*test.js' ],
+    preprocessors: { '/**/*.js': [ 'webpack', 'sourcemap' ] },
     webpack: {
       watch: true,
       devtool: 'inline-source-map',
@@ -17,23 +13,26 @@ module.exports = function (config) {
       module: {
         loaders: [
           {
-            test: /\.js$/, loader: 'istanbul-instrumenter-loader',
-            include: [path.resolve('lib/'), path.resolve('poller.js')],
+            test: /\.js$/,
+            loader: 'istanbul-instrumenter-loader',
+            include: [ path.resolve('lib/'), path.resolve('poller.js') ],
             exclude: /(test|node_modules|bower_components)\//
           }
         ]
       },
-      plugins: [new RewirePlugin()]
+      plugins: [ new RewirePlugin() ]
     },
-    webpackServer: {
-      quiet: true,
-      noInfo: true
-    },
+    webpackServer: { quiet: true, noInfo: true },
     reporters: [ 'progress', 'coverage-istanbul', 'coverage' ],
     coverageIstanbulReporter: {
       reports: [ 'text-summary' ],
       fixWebpackSourcePaths: true
     },
-    browsers: ['Chrome']
+    coverageReporter: {
+      check: {
+        global: { statements: 95, lines: 95, functions: 95, branches: 80 }
+      }
+    },
+    browsers: [ 'Chrome' ]
   })
 }
