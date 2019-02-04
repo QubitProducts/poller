016d23aca1f2cd62e6d07a02242243be895d3ca3
alanclarke <alan@qubit.com>
2019-02-04 09:14:48 +0000

    Remove unnecesary .then

--- poller.js
+++ poller.js
@@ -49,12 +49,6 @@ function poller (targets, opts) {
 
     var item = create(targets, deferred.resolve, deferred.reject)
 
-    deferred.promise.then(function () {
-      unregister(item)
-    }, function () {
-      unregister(item)
-    })
-
     start()
 
     return {
